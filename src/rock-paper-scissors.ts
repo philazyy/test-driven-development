12e8b06f303885d461cf30c6d885542ec1929e08
PHIL HÄGELI <phil.haegeli8@gmail.com>
2025-04-14 19:42:26 +0200

    Code für Aufgabe 2

--- src/rock-paper-scissors.ts
+++ src/rock-paper-scissors.ts
@@ -1,4 +1,21 @@
 export function play(player1: string, player2: string): number {
-  console.log(player1, player2);
-  throw new Error("not implemented yet");
+  const moves = ["rock", "paper", "scissors"];
+  const p1 = player1.trim().toLowerCase();
+  const p2 = player2.trim().toLowerCase();
+
+  if (!moves.includes(p1) || !moves.includes(p2)) {
+    throw new Error("Invalid move");
+  }
+
+  if (p1 === p2) return 0;
+
+  if (
+    (p1 === "rock" && p2 === "scissors") ||
+    (p1 === "scissors" && p2 === "paper") ||
+    (p1 === "paper" && p2 === "rock")
+  ) {
+    return 1;
+  }
+
+  return 2;
 }
